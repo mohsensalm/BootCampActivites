cff0e20b2e4f205a412e611e2cdaf9d91b950c8b
mohsen.sl76 <mohsensalmanzade68@gmail.com>
2023-12-17 20:31:34 +0330

    fix

--- TestInClass/Program.cs
+++ TestInClass/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.ComponentModel.Design;
 
 namespace TestInClass
 {
@@ -10,6 +12,38 @@ namespace TestInClass
     {
         static void Main(string[] args)
         {
+            DirectoryInfo inf = new DirectoryInfo("C:\\");
+            FileInfo[] fileINfo = inf.GetFiles();
+
+            var linqx = from n in fileINfo
+                        select n;
+
+            foreach (var item in linqx)
+            {
+                Console.WriteLine(item.FullName);
+            }
+            Console.ReadKey();
+
+            int[] x = { 83, 32, 1,444,321,555,777, 434, 23, 44, 42 };
+
+            var linqs = from n in x
+                        group n by n % 3 into v
+                        select new
+                        {
+                            reinder = v.Key,
+                            numberingroup = v
+                        };
+            foreach (var item in linqs)
+            {
+                Console.WriteLine($"{item.reinder}");
+                var str = "";
+                foreach (var number in item.numberingroup)
+                {
+                    str += number + ",";
+                }
+                Console.WriteLine(  str );
+            }
+            Console.ReadKey();
         }
     }
 }
